2f226ba99c94719fd5d0b8ecb9fb1595ba3e297a
xinsong <425409998@qq.com>
2018-12-18 09:31:37 +0800

    softmax+tanh

--- test_m.py
+++ test_m.py
@@ -4,5 +4,5 @@ import mnist_loader
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
 
-net = network.Network([784, 100, 10])
+net = network.Network([784, 100, 100, 100, 10])
 net.SGD(training_data, 30, 10, 0.1, test_data= test_data)
