32f194ec54c6338183657e0b29c26ec051c415a7
Forest-fall <Forestfalling1@gmail.com>
2019-01-10 09:49:33 +0800

    1.10

--- plt.py
+++ plt.py
@@ -4,23 +4,24 @@ import matplotlib.pyplot as plt
 
 with open("./dnn_loss", "r") as f:
     loss = json.load(f)
-# with open("./dnn_accuracy", "r") as f:
-#     test_accuracy = json.load(f)
+with open("./dnn_accuracy", "r") as f:
+    test_accuracy = json.load(f)
 
 fig = plt.figure()
-ax1 = fig.add_subplot(111)
-ax1.plot(np.arange(len(loss)), loss, color='#6593d3')
-ax1.grid(True)
-ax1.set_xlabel('Epoch')
-ax1.set_title('dnn loss')
-plt.savefig('./dnn_loss.jpg')
 
-# ax2 = fig.add_subplot(212)
-# ax2.plot(np.arange(3), test_accuracy, color='#6593d3')
-# ax2.grid(True)
-# ax2.set_xlabel('Epoch')
-# ax2.set_title('Accuracy on testdata')
-# plt.savefig('./dnn_accuracy.jpg')
+# ax1 = fig.add_subplot(111)
+# ax1.plot(np.arange(len(loss)), loss, color='#6593d3')
+# ax1.grid(True)
+# ax1.set_xlabel('Epoch')
+# ax1.set_title('dnn loss')
+# plt.savefig('./dnn_loss.jpg')
+
+ax = fig.add_subplot(111)
+ax.plot(np.arange(len(test_accuracy)), test_accuracy, color='red')
+ax.grid(True)
+ax.set_xlabel('Epoch')
+ax.set_title('Accuracy on testdata')
+plt.savefig('./dnn_accuracy.jpg')
 
 plt.show()
 
